6fcb798375dad832e5404188e1c6bc7e6eb69263
Lukasz Tekieli <lukasz.tekieli@bmw.de>
2024-11-14 12:53:26 +0100

    Add file percentage

--- init.lua
+++ init.lua
@@ -931,7 +931,7 @@ require('lazy').setup({
       -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
-        return '%2l:%-2v'
+        return '%2l:%-2v %P'
       end
 
       -- ... and there is more!
